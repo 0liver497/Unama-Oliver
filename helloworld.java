8ae4bbd6e5aafd61385d895fce351e4cc5b799e1
0liver497 <olivermatheus368@gmail.com>
2024-08-22 00:51:18 +0000

    finalizacao da aula

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,13 @@
-class helloworld {
+
+class testeidade {
+
     public static void main(String[] args) {
-      System.out.println("Olá mundo!");
+      double janeiro = 15000.0;
+      double fevereiro = 23000.0;
+      double marco = 17000.0;
+
+      double resultado = janeiro + fevereiro + marco;
+
+      System.out.println(resultado);
     }
 }
